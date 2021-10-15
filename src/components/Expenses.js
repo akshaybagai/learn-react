64bcb7cbb0340551f0d39e60b5ee4c5d61877da2
Akshay Bagai <akshay.bagai@instacart.com>
2021-10-15 15:35:57 -0400

    listen to event listener

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,18 @@
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
+
 import './Expenses.css'
 import Card from "./Card";
 
 function Expenses(props) {
 
+    const onFilterChangeEventHandler = (filter) => {
+      console.log(filter);
+    };
+
     return (
         <div>
+            <ExpensesFilter onFilterChange={onFilterChangeEventHandler}/>
             <Card className="expenses">
                 <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
                 <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
