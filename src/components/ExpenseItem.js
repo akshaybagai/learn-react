c4cf9868bead0b6309cac82938d564f98f5aaede
Akshay Bagai <akshay.bagai@instacart.com>
2021-10-15 15:53:59 -0400

    made expense item a stateless component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,22 +5,16 @@ import './ExpenseItem.css'
 import Card from "./Card";
 
 function ExpenseItem(props) {
-    //use state always returns two values first the current value, second to update the value
-    const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () => {
-        console.log("Clicked");
-        setTitle('Updated');
-    };
-
+    //as there's no state this is a stateless component.
+    // so this is what just presents the data to user
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">{props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
