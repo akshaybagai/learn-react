2a3e8447f6c249425edba5e54bba852aed093c91
Akshay Bagai <akshay.bagai@instacart.com>
2021-10-15 15:41:40 -0400

    listen to event listener

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
 
@@ -6,13 +7,16 @@ import Card from "./Card";
 
 function Expenses(props) {
 
-    const onFilterChangeEventHandler = (filter) => {
-      console.log(filter);
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const onFilterChangeEventHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+        console.log(selectedYear);
     };
 
     return (
         <div>
-            <ExpensesFilter onFilterChange={onFilterChangeEventHandler}/>
+            <ExpensesFilter onFilterChange={onFilterChangeEventHandler} selected={filteredYear}/>
             <Card className="expenses">
                 <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
                 <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
